636c13335613a1efdcac7326c2b22586d7cdc390
Betoml5 <betoml5@hotmail.com>
2022-05-29 18:48:33 -0500

    Agregar archivos de proyecto.

---
+++ TurismoApp/TurismoApp/Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurismoApp.Models
+{
+    public class Guest
+    {
+
+        public string Name { get; set; }
+        public int RoomNumber { get; set; }
+        public string Plan { get; set; }
+
+
+    }
+}
---
+++ TurismoApp/TurismoApp/ViewModels/GuestViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using TurismoApp.Models;
+
+namespace TurismoApp.ViewModels
+{
+    public  class GuestViewModel: INotifyPropertyChanged
+    {
+
+
+
+
+        public Guest Guest { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
