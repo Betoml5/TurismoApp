2e76f81af5c0a440ede9e2a5e1ba7a98d793b6b1
Betoml5 <betoml5@hotmail.com>
2022-05-30 23:23:06 -0500

    Add template to city

--- TurismoApp/TurismoApp/App.xaml.cs
+++ TurismoApp/TurismoApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace TurismoApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Views.Home());
         }
 
         protected override void OnStart()
---
+++ TurismoApp/TurismoApp/Models/City.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurismoApp.Models
+{
+    public class City
+    {
+
+        public string Name { get; set; }
+        public bool isFav { get; set; }
+
+        public string Type { get; set; }
+
+        public string Image { get; set; }
+
+        public double AvgPrice { get; set; }
+    }
+}
